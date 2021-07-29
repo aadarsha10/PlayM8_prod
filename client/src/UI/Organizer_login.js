d9b757afdf6ac181fe616b75cc49de0cca9c0710
Aadarsha Man Shrestha <a.shrestha998@gmail.com>
2021-07-30 01:37:01 +0545

    For sprint 2 release

--- client/src/UI/Organizer_login.js
+++ client/src/UI/Organizer_login.js
@@ -4,7 +4,7 @@ import "../scss/login.scss";
 import { Form } from "react-bootstrap";
 // import Alert from 'react-bootstrap/Alert'
 import axios from "axios";
-import LandingNavbar from "../Components/LandingNavbar";
+import LandingNavbar from "./LandingNavbar";
 
 export default function Login() {
   const [Username, setUsername] = useState("");
